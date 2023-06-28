acdd7981c21dee1797c79c131aa103648eeaa821
victorbenedict <103329136+victorbenedict@users.noreply.github.com>
2023-06-28 22:21:10 +0800

    Add scoring feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hands = ['rock', 'paper', 'scissors'];
 const hands_icon = ['✊', '✋', '✌'];
+let number_of_wins = 5;
+let result;
 
 function Player(name, hands, score = 0){
 	this.name = name;
@@ -17,11 +19,16 @@ Player.prototype.hands_icon = function(index){
 const human = new Player('Human')
 const computer = new Player('computer')
 
-function gameStart(){
+function game(){
 	computer.hands = getRandomHands();
 	getWinner(human.hands, computer.hands)
-	console.log(`Human: ${human.score}`)
-	console.log(`Computer: ${computer.score}`)
+	if ( human.score == number_of_wins || computer.score == number_of_wins ) {
+		const buttons = document.querySelectorAll('.button_container button');
+		buttons.forEach(button => {
+			button.disabled = true;
+			result = human.score == number_of_wins ? 'You are the winner!' : 'Computer won the match.'
+		});
+	}
 }
 
 function getRandomHands(){
@@ -30,22 +37,16 @@ function getRandomHands(){
 
 function getWinner(){
 	const a = `${human.hands}${computer.hands}`;
-	const draw = `${a} Draw!`;
-	const playerWins = `${a} Player wins!`;
-	const computerWins = `${a} Computer wins!`;
 	if (human.hands == computer.hands){
-		console.log(draw);
-		return ''
+		return result = 'Draw'
 	}
 	else if (a == 0o2 || a == 10 || a == 21){
 		human.addScore();
-		console.log(playerWins);
-		return ''
+		return result = 'Player won the round!'
 	}
 	else{
 		computer.addScore();
-		console.log(computerWins);
-		return ''
+		return result = 'Computer won the round!'
 	}
 }
 
@@ -59,24 +60,27 @@ function renderDisplay(index){
 	const div_computer_score = document.querySelector('.computer_score');
 	div_player_score.textContent = `Player: ${human.score}`
 	div_computer_score.textContent = `Computer: ${computer.score}`
+
+	const div_result = document.querySelector('.display_result');
+	div_result.textContent = result;
 }
 
 //0-rock, 1-paper, 2-scissors
 document.querySelector('.btn_rock').addEventListener('click', function(){
 	human.hands = 0;
-	gameStart();
+	game();
 	renderDisplay(0);
 });
 
 document.querySelector('.btn_paper').addEventListener('click', function(){
 	human.hands = 1;
-	gameStart();
+	game();
 	renderDisplay(1);
 });
 
 document.querySelector('.btn_scissors').addEventListener('click', function(){
 	human.hands = 2;
-	gameStart();
+	game();
 	renderDisplay(2);
 });
 
